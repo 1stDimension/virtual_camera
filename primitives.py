4c01f2d96c4a1bf315bdb96c6b58121dc46d30ed
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-03-29 18:39:29 +0200

    Adding class representing edge

--- primitives.py
+++ primitives.py
@@ -5,3 +5,10 @@ class Node(object):
     self.z = coordinates[2];
   def __str__(self):
     return f"x = {self.x}|y ={self.y}|z = {self.z}"
+
+class Edge(object):
+  def __init__(self, begin : Type[Node], end: Type[Node]):
+    self.begin = begin
+    self.end = end
+  def __str__(self):
+    return f"begin = ({self.begin}), end = ({self.end})"
