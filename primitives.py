7bc00d98dc481d9b5f56cb7b717c684664a281d3
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-03-30 20:36:24 +0200

    reformat and adding numpy

--- primitives.py
+++ primitives.py
@@ -1,35 +1,40 @@
 from typing import Type, List
+import numpy as np
+
 
 class Node(object):
-  def __init__(self, coordinates : list):
-    self.x = coordinates[0];
-    self.y = coordinates[1];
-    self.z = coordinates[2];
-  def __str__(self):
-    return f"x = {self.x}|y ={self.y}|z = {self.z}"
+    def __init__(self, coordinates: list):
+        self.coordinates = np.array(coordinates)
+
+    def __str__(self):
+        return f"coordinates = {self.coordinates}"
+
 
 class Edge(object):
-  def __init__(self, begin : Type[Node], end: Type[Node]):
-    self.begin = begin
-    self.end = end
-  def __str__(self):
-    return f"begin = ({self.begin}), end = ({self.end})"
+    def __init__(self, begin: Type[Node], end: Type[Node]):
+        self.begin = begin
+        self.end = end
+
+    def __str__(self):
+        return f"begin = ({self.begin}), end = ({self.end})"
+
 
 class WorldObject(object):
-  def __init__(self):
-    self.nodes : list = []
-    self.edges : list = []
-  
-  def add_nodes(self, nodes : List[Type[Node]]):
-    for node in nodes:
-      self.nodes.append(node)
+    def __init__(self):
+        self.nodes: list = []
+        self.edges: list = []
 
-  def add_edges(self, edges : List[Type[Edge]]):
-    for edge in edges:
-      self.edges.append(edge)
+    def add_nodes(self, nodes: List[Type[Node]]):
+        for node in nodes:
+            self.nodes.append(node)
 
-  def print_nodes(self):
-    print(self.nodes)
+    def add_edges(self, edges: List[Type[Edge]]):
+        for edge in edges:
+            self.edges.append(edge)
 
-  def print_edges(self):
-    print(self.edges)
+    def print_nodes(self):
+        print(self.nodes)
+
+    def print_edges(self):
+        print(self.edges)
+
